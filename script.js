bbb13609a3a62828d7ce487b9c9f93895ee77b2d
rigel X <paroxsymalrage@gmail.com>
2022-09-04 17:59:16 -0300

    opium*

---
+++ script.js
@@ -0,0 +1,34 @@
+const player = document.querySelector('.player');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    player.classList.add('jump');
+
+    setTimeout(() => {
+        player.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() =>{
+
+    const pipePosition = pipe.offsetLeft;
+    const playerPosition = +window.getComputedStyle(player).bottom.replace('px', '');
+
+    if(pipePosition <= 96 && playerPosition < 80 && pipePosition > 0){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        player.style.animation = 'none';
+        player.style.bottom = `${playerPosition}px`;
+
+        player.src = 'livderrota.png';
+        player.style.width = '95px';
+        player.style.marginLeft = '0px';
+
+        clearInterval(loop);
+    }
+} , 10)
+
+
+document.addEventListener('keydown', jump);
